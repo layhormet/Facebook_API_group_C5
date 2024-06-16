f5466648b2a5c5547ac892f8c4909075538c5389
PHANIT.CHEA <PHANIT.CHEA@PNC.LAN>
2024-06-16 17:23:45 +0700

    add-friend,accepted and decline friends #18

---
+++ app/Models/friendShip.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friendship extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'friend_id',
+        'status',
+    ];
+
+
+    public function friends()
+    {
+        return $this->belongsToMany(User::class, 'friendships', 'user_id', 'friend_id')
+            ->withPivot('status')
+            ->withTimestamps();
+    }
+
+    public function pendingFriendRequests()
+    {
+        return $this->belongsToMany(User::class, 'friendships', 'friend_id', 'user_id')
+            ->wherePivot('status', 'pending')
+            ->withPivot('status')
+            ->withTimestamps();
+    }
+}
